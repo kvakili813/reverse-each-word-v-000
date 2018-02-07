74700e2db032313471c6707a54650bb06404d318
Kayvan Vakili <kvakili813@gmail.com>
2018-02-07 05:05:34 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,12 +6,12 @@ end
 backward_sentence.join(' ')
 end
 
-def reverse_each_word(sentence2)
-  backward_sentence = []
-  sentence2.split.collect do |word|
-    backward_sentence << word.reverse
-end
-backward_sentence.join(' ')
-end
+#def reverse_each_word(sentence2)
+  #backward_sentence = []
+  #sentence2.split.collect do |word|
+    #backward_sentence << word.reverse
+#end
+#backward_sentence.join(' ')
+#end
 
 reverse_each_word(sentence1)

a1f18394396f3ffe356fa675e59f648116de6648
Kayvan Vakili <kvakili813@gmail.com>
2018-02-07 04:43:00 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(sentence1)
   backward_sentence = []
   sentence1.split.each do |word|
-    backward_sentence << "word.reverse"
+    backward_sentence << word.reverse
 end
 backward_sentence
 end
